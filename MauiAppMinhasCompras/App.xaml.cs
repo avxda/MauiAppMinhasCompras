6aff8941c854f4a8b2521a97ffa1594d83d1f332
Kauan Felipe <KauanFelipe.Personal@outlook.com>
2026-08-15 15:56:40 -0300

    Add project files.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
